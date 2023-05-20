760c3b7808f47e68b8ea1546713b99ae06fc3f79
AyaMahmoud253 <119099432+AyaMahmoud253@users.noreply.github.com>
2023-05-20 15:18:03 +0300

    Edit

--- CosineSimilarity/src/Ass2/InvertedIndex.java
+++ CosineSimilarity/src/Ass2/InvertedIndex.java
@@ -1,182 +1,199 @@
 package Ass2;
-import java.util.Comparator;
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.LinkedHashMap;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Map;
-import java.util.Scanner;
-import java.util.Set;
 
-class Posting {
-    int docId;
-    int tf;
-    Posting next;
+import java.io.*;
+import java.util.*;
 
-    public Posting(int docId, int tf) {
-        this.docId = docId;
-        this.tf = tf;
-        this.next = null;
-    }
-}
 
-class DictEntry {
-    int docFreq;
-    int termFreq;
-    Posting pList;
+public class InvertedIndex {
+    private Map<String, List<String>> index;
 
-    public DictEntry() {
-        this.docFreq = 0;
-        this.termFreq = 0;
-        this.pList = null;
-    }
-}
-
-class InvertedIndex1 {
-    Map<String, DictEntry> index;
-
-    public InvertedIndex1() {
-        this.index = new HashMap<>();
+    public InvertedIndex() {
+        index = new HashMap<>();
     }
 
-    public void buildIndex(String[] filenames) throws IOException {
-        for (int i = 0; i < filenames.length; i++) {
-            int docId = i;
-            BufferedReader reader = new BufferedReader(new FileReader(filenames[i]));
-            String line = null;
-            while ((line = reader.readLine()) != null) {
-                String[] terms = line.split("\\W+");
-                for (String term : terms) {
-                    term = term.toLowerCase();
-                    if (term.length() == 0)
-                        continue;
-                    DictEntry entry = index.getOrDefault(term, new DictEntry());
-                    entry.termFreq++;
-                    Posting curr = entry.pList;
-                    Posting prev = null;
-                    boolean found = false;
-                    while (curr != null) {
-                        if (curr.docId == docId) {
-                            curr.tf++;
-                            found = true;
-                            break;
-                        }
-                        prev = curr;
-                        curr = curr.next;
+    public class Pair<K, V> {
+        private K key;
+        private V value;
+
+        public Pair(K key, V value) {
+            this.key = key;
+            this.value = value;
+        }
+
+        public K getKey() {
+            return key;
+        }
+
+        public void setKey(K key) {
+            this.key = key;
+        }
+
+        public V getValue() {
+            return value;
+        }
+
+        public void setValue(V value) {
+            this.value = value;
+        }
+    }
+    public void buildIndex(String[] files) {
+        for (String file : files) {
+            try (Scanner scanner = new Scanner(new File(file))) {
+                while (scanner.hasNext()) {
+                    String word = scanner.next().toLowerCase();
+                    if (!index.containsKey(word)) {
+                        index.put(word, new ArrayList<>());
                     }
-                    if (!found) {
-                        entry.docFreq++;
-                        Posting posting = new Posting(docId, 1);
-                        if (prev == null) {
-                            entry.pList = posting;
-                        } else {
-                            prev.next = posting;
-                        }
+                    if (!index.get(word).contains(file)) {
+                        index.get(word).add(file);
                     }
-                    index.put(term, entry);
                 }
+            } catch (FileNotFoundException e) {
+                System.out.println("File not found: " + file);
             }
-            reader.close();
         }
     }
 
-    public Map<Integer, Double> cosineSimilarity(String query) throws IOException {
-        String[] terms = query.split("\\W+");
-        Set<String> uniqueTerms = new HashSet<>();
-        for (String term : terms) {
-            term = term.toLowerCase();
-            if (term.length() > 0) {
-                uniqueTerms.add(term);
+    public Pair<List<String>, Map<String, Double>> search(String[] query) {
+        Map<String, Integer> queryVector = new HashMap<>();
+        for (String word : query) {
+            word = word.toLowerCase();
+            if (queryVector.containsKey(word)){
+                queryVector.put(word, queryVector.get(word) + 1);
+            } else {
+                queryVector.put(word, 1);
             }
         }
-        int numDocs = 10;
-        double[] scores = new double[numDocs];
-        for (String term : uniqueTerms) {
-            if (index.containsKey(term)) {
-                DictEntry entry = index.get(term);
-                int docFreq = entry.docFreq;
-                int termFreq = entry.termFreq;
-                double idf = Math.log10((double) numDocs / docFreq);
-                Posting p = entry.pList;
-                while (p != null) {
-                    double tf = 1 + Math.log10((double) p.tf);
-                    double wtd = tf * idf;
-                    scores[p.docId] += wtd;
-                    p = p.next;
+       
+        Map<String, Double> cosineSimilarities = new HashMap<>();
+        for (String file : index.values().stream().flatMap(List::stream).distinct().toArray(String[]::new)) {
+            Map<String, Integer> documentVector = new HashMap<>();
+           try (Scanner scanner = new Scanner(new File(file))) {
+                while (scanner.hasNext()) {
+                    String word = scanner.next().toLowerCase();
+                    if (documentVector.containsKey(word)) {
+                        documentVector.put(word, documentVector.get(word) + 1);
+                    } else {
+                        documentVector.put(word, 1);
+                    }
                 }
+            } catch (FileNotFoundException e) {
+                System.out.println("File not found: " + file);
             }
+
+            double dotProduct = 0.0;
+            double queryMagnitude = 0.0;
+            double documentMagnitude = 0.0;
+
+            for (Map.Entry<String, Integer> entry : queryVector.entrySet()) {
+                String word = entry.getKey();
+                int queryFrequency = entry.getValue();
+                int documentFrequency = documentVector.getOrDefault(word, 0);
+                dotProduct += queryFrequency * documentFrequency;
+                queryMagnitude += queryFrequency * queryFrequency;
+            }
+
+            for (Map.Entry<String, Integer> entry : documentVector.entrySet()) {
+                int frequency = entry.getValue();
+                documentMagnitude +=frequency * frequency;
+            }
+
+            double cosineSimilarity = dotProduct / (Math.sqrt(queryMagnitude) * Math.sqrt(documentMagnitude));
+            cosineSimilarities.put(file, cosineSimilarity);
         }
-        double[] docLengths = new double[numDocs];
-        for (int i = 0; i < numDocs; i++) {
-            BufferedReader reader = new BufferedReader(new FileReader(i + ".txt"));
-            String line = null;
-            while ((line = reader.readLine()) != null) {
-                String[] termsInDoc = line.split("\\W+");
-                for (String term : termsInDoc) {
-                    term = term.toLowerCase();
-                    if (term.length() == 0)
-                        continue;
-                    int ttf = index.getOrDefault(term, new DictEntry()).pList == null ? 0 : index.get(term).pList.tf;
-                    double tf = 1 + Math.log10((double) ttf);
-                    docLengths[i] += Math.pow(tf, 2);
+
+        List<String> rankedFiles = new ArrayList<>(cosineSimilarities.keySet());
+        rankedFiles.sort((file1, file2) -> Double.compare(cosineSimilarities.get(file2), cosineSimilarities.get(file1)));
+
+        return new Pair<>(rankedFiles, cosineSimilarities);
+    }
+    public Map<Pair<String, String>, Double> calculateAllSimilarities() {
+        Map<Pair<String, String>, Double> similarities = new HashMap<>();
+
+        // Calculate similarity for each pair of documents
+        for (String file1 : index.values().stream().flatMap(List::stream).distinct().toArray(String[]::new)) {
+            for (String file2 : index.values().stream().flatMap(List::stream).distinct().toArray(String[]::new)) {
+                if (file1.equals(file2)) {
+                    continue; // Skip the same document
                 }
-            }
-            reader.close();
-        }
-        for (int i = 0; i < numDocs; i++) {
-            if (docLengths[i] > 0) {
-                scores[i] /= Math.sqrt(docLengths[i]);
+
+                // Calculate similarity between file1 and file2
+                double similarity = calculateSimilarity(file1, file2);
+                similarities.put(new Pair<>(file1, file2), similarity);
             }
         }
-        Map<Integer, Double> resultMap = new HashMap<>();
-        for (int i = 0; i < numDocs; i++) {
-            resultMap.put(i, scores[i]);
+
+        return similarities;
+    }
+
+    private double calculateSimilarity(String file1, String file2) {
+        Map<String, Integer> document1Vector = getDocumentVector(file1);
+        Map<String, Integer> document2Vector = getDocumentVector(file2);
+
+        double dotProduct = 0.0;
+        double document1Magnitude = 0.0;
+        double document2Magnitude = 0.0;
+
+        // Calculate dot product and magnitudes
+        for (Map.Entry<String, Integer> entry : document1Vector.entrySet()) {
+            String word = entry.getKey();
+            int document1Frequency = entry.getValue();
+            int document2Frequency = document2Vector.getOrDefault(word, 0);
+            dotProduct += document1Frequency * document2Frequency;
+            document1Magnitude += document1Frequency * document1Frequency;
         }
-        return resultMap;
+
+        for (Map.Entry<String, Integer> entry : document2Vector.entrySet()) {
+            int frequency = entry.getValue();
+            document2Magnitude += frequency * frequency;
+        }
+
+        // Calculate cosine similarity
+        return dotProduct / (Math.sqrt(document1Magnitude) * Math.sqrt(document2Magnitude));
+    }
+
+    private Map<String, Integer> getDocumentVector(String file) {
+        Map<String, Integer> documentVector = new HashMap<>();
+
+        try (Scanner scanner = new Scanner(new File(file))) {
+            while (scanner.hasNext()) {
+                String word = scanner.next().toLowerCase();
+                documentVector.put(word, documentVector.getOrDefault(word, 0) + 1);
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found: " + file);
+        }
+
+        return documentVector;
+    }
+
+
+    public static void main(String[] args) {
+        //String[] files = {"0.txt", "1.txt", "2.txt","3.txt", "4.txt", "5.txt", "6.txt", "7.txt", "8.txt", "9.txt"};
+    	String[] files = {"0.txt", "1.txt"};
+        InvertedIndex index = new InvertedIndex();
+        index.buildIndex(files);
+       
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter query: ");
+        String userInput = scanner.nextLine();
+
+        String[] query = userInput.split(" ");
+     // ...
+        Pair<List<String>, Map<String, Double>> result = index.search(query);
+        System.out.println("Ranked files:");
+        for (String file : result.getKey()) {
+            System.out.println(file );
+        }
+
+        // Calculate all document similarities
+        Map<Pair<String, String>, Double> allSimilarities = index.calculateAllSimilarities();
+        System.out.println("All document similarities:");
+        for (Map.Entry<Pair<String, String>, Double> entry : allSimilarities.entrySet()) {
+            Pair<String, String> documentPair = entry.getKey();
+            double similarity = entry.getValue();
+            System.out.println(documentPair.getKey() + " and " + documentPair.getValue() + " Similarity: " + similarity);
+        }
+
     }
 }
-
-	public class InvertedIndex {
-		public static void main(String[] args) throws IOException {
-		    String[] filenames = {"0.txt","1.txt","2.txt","3.txt","4.txt","5.txt","6.txt","7.txt","8.txt","9.txt"};
-		    InvertedIndex1 index = new InvertedIndex1();
-		    index.buildIndex(filenames);
-
-		    Scanner sc= new Scanner(System.in);
-		    System.out.print("Enter a query: ");
-		    String query = sc.nextLine();
-
-		    Map<Integer, Double> resultMap = index.cosineSimilarity(query);
-	        
-	        // Sort the map by value in descending order
-	        Map<Integer, Double> sortedMap = sortByValue(resultMap);
-	        
-		    int k = 3;
-		    for (Map.Entry<Integer, Double> entry : sortedMap.entrySet()) {
-		        if (k-- == 0) break; 
-		        System.out.println((10 - k) + ". " + filenames[entry.getKey()]);
-		    }
-		}
-
-	    public static Map<Integer, Double> sortByValue(Map<Integer, Double> map) {
-	        List<Map.Entry<Integer, Double>> list = new LinkedList<>(map.entrySet());
-	        Collections.sort(list, new Comparator<Map.Entry<Integer, Double>>() {
-	            public int compare(Map.Entry<Integer, Double> o1, Map.Entry<Integer, Double> o2) {
-	                return (o2.getValue()).compareTo(o1.getValue());
-	            }
-	        });
-
-	       Map<Integer, Double> result = new LinkedHashMap<>();
-	        for (Map.Entry<Integer, Double> entry : list) {
-	            result.put(entry.getKey(), entry.getValue());
-	        }
-	        return result;
-	    }
-	}
